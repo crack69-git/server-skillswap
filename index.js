a9283a16fb7ec4ee3592c1d5f7a148c1b3f320d1
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-07-07 12:09:37 +0600

    added vercel

--- index.js
+++ index.js
@@ -644,10 +644,10 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!",
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
